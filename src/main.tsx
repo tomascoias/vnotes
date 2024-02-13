c5d3df8eb0f14ac767a4166acb6f3e6837ffd4e6
tomascoias <tomascoias04@gmail.com>
2024-02-13 10:25:05 +0000

    add: edit note

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster position="top-right" richColors/>
+    <Toaster position="top-right" richColors />
   </React.StrictMode>,
 )
